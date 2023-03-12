55375ca26b1cf1d3880e2eb015167478b13933c5
chorockuin <chorockuin@gmail.com>
2023-03-12 21:08:35 +0900

    Add colab_0
    -------------------------------------------------|
    ------------------------------------------------------------------------------|
    * Subject Rule
      - English only
      - Capitalize first character
      - 50 characters or less
      - Don't use '.'
      - Begin with below word only
        - Create : feature
        - Refactor : feature and not testcase
        - Update : feature and testcase
        - Delete : feature
        - Fix : bugs
    * Body Rule
      - 79 characters or less per line
      - Describe 'what' and 'why' not 'how'

--- cs224w/0.py
+++ cs224w/0.py
@@ -104,3 +104,140 @@ print(pr)
 import torch
 print("PyTorch has version {}".format(torch.__version__))
 
+################### Visualization
+
+# Helper function for visualization.
+import torch
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Visualization function for NX graph or PyTorch tensor
+def visualize(h, color, epoch=None, loss=None, accuracy=None):
+    plt.figure(figsize=(7,7))
+    plt.xticks([])
+    plt.yticks([])
+
+    if torch.is_tensor(h):
+        h = h.detach().cpu().numpy()
+        plt.scatter(h[:, 0], h[:, 1], s=140, c=color, cmap="Set2")
+        if epoch is not None and loss is not None and accuracy['train'] is not None and accuracy['val'] is not None:
+            plt.xlabel((f'Epoch: {epoch}, Loss: {loss.item():.4f} \n'
+                       f'Training Accuracy: {accuracy["train"]*100:.2f}% \n'
+                       f' Validation Accuracy: {accuracy["val"]*100:.2f}%'),
+                       fontsize=16)
+    else:
+        nx.draw_networkx(h, pos=nx.spring_layout(h, seed=42), with_labels=False,
+                         node_color=color, cmap="Set2")
+    plt.show()
+
+################### Dataset
+
+from torch_geometric.datasets import KarateClub
+
+dataset = KarateClub()
+print(f'Dataset: {dataset}:')
+print('======================')
+print(f'Number of graphs: {len(dataset)}')
+print(f'Number of features: {dataset.num_features}')
+print(f'Number of classes: {dataset.num_classes}')
+
+data = dataset[0]  # Get the first graph object.
+
+print(data)
+print('==============================================================')
+
+# Gather some statistics about the graph.
+print(f'Number of nodes: {data.num_nodes}')
+print(f'Number of edges: {data.num_edges}')
+print(f'Average node degree: {(2*data.num_edges) / data.num_nodes:.2f}')
+print(f'Number of training nodes: {data.train_mask.sum()}')
+print(f'Training node label rate: {int(data.train_mask.sum()) / data.num_nodes:.2f}')
+print(f'Contains isolated nodes: {data.has_isolated_nodes()}')
+print(f'Contains self-loops: {data.has_self_loops()}')
+print(f'Is undirected: {data.is_undirected()}')
+
+print(data.edge_index.T)
+
+################### Data
+
+print(data)
+
+################### Edge Index
+
+edge_index = data.edge_index
+print(edge_index.t())
+
+from torch_geometric.utils import to_networkx
+
+G = to_networkx(data, to_undirected=True)
+visualize(G, color=data.y)
+
+################### Implementing Graph Neural Networks (GNNs)
+
+import torch
+from torch.nn import Linear
+from torch_geometric.nn import GCNConv
+
+class GCN(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        torch.manual_seed(1234)
+        self.conv1 = GCNConv(dataset.num_features, 4)
+        self.conv2 = GCNConv(4, 4)
+        self.conv3 = GCNConv(4, 2)
+        self.classifier = Linear(2, dataset.num_classes)
+
+    def forward(self, x, edge_index):
+        h = self.conv1(x, edge_index)
+        h = h.tanh()
+        h = self.conv2(h, edge_index)
+        h = h.tanh()
+        h = self.conv3(h, edge_index)
+        h = h.tanh()  # Final GNN embedding space.
+        
+        # Apply a final (linear) classifier.
+        out = self.classifier(h)
+
+        return out, h
+
+model = GCN()
+print(model)
+
+_, h = model(data.x, data.edge_index)
+print(f'Embedding shape: {list(h.shape)}')
+
+visualize(h, color=data.y)
+
+import time
+
+criterion = torch.nn.CrossEntropyLoss()  # Define loss criterion.
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)  # Define optimizer.
+
+def train(data):
+    optimizer.zero_grad()  # Clear gradients.
+    out, h = model(data.x, data.edge_index)  # Perform a single forward pass.
+    loss = criterion(out[data.train_mask], data.y[data.train_mask])  # Compute the loss solely based on the training nodes.
+    loss.backward()  # Derive gradients.
+    optimizer.step()  # Update parameters based on gradients.
+
+    accuracy = {}
+    # Calculate training accuracy on our four examples
+    predicted_classes = torch.argmax(out[data.train_mask], axis=1) # [0.6, 0.2, 0.7, 0.1] -> 2
+    target_classes = data.y[data.train_mask]
+    accuracy['train'] = torch.mean(
+        torch.where(predicted_classes == target_classes, 1, 0).float())
+    
+    # Calculate validation accuracy on the whole graph
+    predicted_classes = torch.argmax(out, axis=1)
+    target_classes = data.y
+    accuracy['val'] = torch.mean(
+        torch.where(predicted_classes == target_classes, 1, 0).float())
+
+    return loss, h, accuracy
+
+for epoch in range(500):
+    loss, h, accuracy = train(data)
+    # Visualize the node embeddings every 10 epochs
+    if epoch % 10 == 0:
+        visualize(h, color=data.y, epoch=epoch, loss=loss, accuracy=accuracy)
+        time.sleep(0.3)
